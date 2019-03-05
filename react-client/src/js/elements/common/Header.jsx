5aa50ce95cedf187d7e2a95a8994dba1f8fee917
Albert Panesse <albert.panesse@gmail.com>
2019-03-05 13:59:04 +0000

    homepage design impl #1

--- react-client/src/js/elements/common/Header.jsx
+++ react-client/src/js/elements/common/Header.jsx
@@ -19,22 +19,29 @@ class Header extends React.Component {
 
 	render() {
 		return (
-			<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm tshop ts-header">
-				<h5 className="my-0 mr-md-auto font-weight-normal ts-title">TShirtShop</h5>
-				<nav className="my-2 my-md-0 mr-md-3">
-					{this.renderDepartments()}
-				</nav>
-
-				<form className="form-inline mt-2 mt-md-0">
-					<div className="input-group mb-3">
-						<input type="text" className="form-control" placeholder="Search ..." />
-						<div className="input-group-append">
-							<span className="input-group-text"><i className="fas fa-search" /></span>
-						</div>
-					</div>
-				</form>
+			<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm ts-header">
+				<div className="ts-header-part1">
+					<h5 className="my-0 font-weight-normal ts-title"><a href="/">TShirtShop</a></h5>
+				</div>
 				
-				<Link to="/cart" className="btn"><i className="fas fa-shopping-bag" /></Link>
+				<div className="ts-header-part2">
+					<nav className="my-2 my-md-0 mr-md-3 ts-top-nav">
+						{this.renderDepartments()}
+					</nav>
+				</div>
+				
+				<div className="ts-header-part3">
+					<form className="form-inline mt-2 mt-md-0">
+						<div className="input-group mb-3">
+							<input type="text" className="form-control" placeholder="Search ..." />
+							<div className="input-group-append">
+								<span className="input-group-text"><i className="fas fa-search" /></span>
+							</div>
+						</div>
+					</form>
+				</div>
+				
+				<div className="ts-header-part4"><Link to="/cart" className="btn"><i className="fas fa-shopping-bag" /></Link></div>
 			</div>
 		);
 	}
